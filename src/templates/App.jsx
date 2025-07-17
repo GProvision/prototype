036754107007abc5de0666153781bf00e37531c4
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-07-17 18:52:01 -0300

    Upadte Redirect Home

--- src/templates/App.jsx
+++ src/templates/App.jsx
@@ -1,23 +1,21 @@
 import { useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import User from "../components/User";
-import { Outlet, useNavigate } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import useUser from "../context/useUser";
 import { Toaster } from "react-hot-toast";
 const App = () => {
-  const { user } = useUser();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!user) navigate("/auth");
-  }, [user]);
-  return (
-    <>
-      <Sidebar />
-      <User />
-      <Outlet />
-      <Toaster position="bottom-right" reverseOrder={false} />
-    </>
-  );
+  const user = useUser((state) => state.user);
+  if (!user || !user?.user) return <Navigate to="/auth" />;
+  if (user && user?.user)
+    return (
+      <>
+        <Sidebar />
+        <User />
+        <Outlet />
+        <Toaster position="bottom-right" reverseOrder={false} />
+      </>
+    );
 };
 
 export default App;
